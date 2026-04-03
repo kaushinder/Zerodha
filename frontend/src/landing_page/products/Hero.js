31ef2fe00b1e700edaf4f6fc8110194e74ad5081
kaushinder <kaushindersinghraghav@gmail.com>
2026-04-04 00:48:37 +0530

    Enhance Hero component with product details and improved layout

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="text-center mt-5">
+        <h1>Zerodha Products</h1>
+        <h3 className="text-muted mt-3 fs-4">
+          Sleek, modern, and intuitive trading platforms
+        </h3>
+        <p className="mt-3">
+          Check out our{" "}
+          <a style={{ textDecoration: "none" }} href="/investment-offerings">
+            investment offerings <i class="fa-solid fa-arrow-right-long"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
